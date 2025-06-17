a76b146ef3b32ba9238954f5c8c96e0fc4f94dca
Andrey Gheno Piekas <andrey.piekas@gmail.com>
2025-06-16 21:24:09 -0300

    2

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Configurações adicionais se necessário
+config.resolver.sourceExts = ['jsx', 'js', 'ts', 'tsx', 'json'];
+
 module.exports = config;
